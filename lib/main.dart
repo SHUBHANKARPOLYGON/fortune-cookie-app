5ff8c5c362a5e7a86e4e3275546541e9b6562333
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-01-23 22:40:20 +0530

    8th commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-   MyHomePage({super.key});
+  MyHomePage({super.key});
 
   String _currentFortune = "1";
-  
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -51,41 +49,34 @@ class _MyHomePageState extends State<MyHomePage> {
     "You will have a great day the day after the day after the day after the day after the day after the day after the day after the day after the day after tomorrow.",
   ];
 
-  void _randomFortune(){
+  void _randomFortune() {
     var random = Random();
     int fortune = random.nextInt(_fortuneList.length);
     setState(() {
       _currentFortune = _fortuneList[fortune];
     });
 
-  
-    print (_currentFortune);
+    print(_currentFortune);
   }
 
   void _incrementCounter() {
     setState(() {
-     
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text("Fortune Cookie"),
       ),
       body: Center(
-      
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             Text(
+            Text(
               "Your Fortune is:",
             ),
             Text(
