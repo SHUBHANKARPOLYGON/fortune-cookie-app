867a27b97f112ca8968ec5abf307acf3defca270
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-01-28 19:17:04 +0530

    learn commit

---
+++ lib/learn.dart
@@ -0,0 +1,10 @@
+// Creating another function and call it in main function.
+void main(){
+  sayMyName();   // } Called created function in main function
+}
+
+void sayMyName(){
+  var name = "Shubhankar Tiwary";
+  int age = 21;
+  print("My name is $name and I am $age years old.");
+}
